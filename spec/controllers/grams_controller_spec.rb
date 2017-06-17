44bde374ac22830b99e04f4edf84022533dfb321
fazaton <fazaton@gmail.com>
2017-06-17 10:23:01 +0000

    gosh

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,34 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
-    describe "comments#create action" do
-        it "should allow users to create comments on grams" do
-          gram = FactoryGirl.create(:gram)
-        
-          user = FactoryGirl.create(:user)
-          sign_in user
-        
-        post :create, params: { gram_id: gram.id, comment: { message: 'awesome gram' } }
-
-          expect(response).to redirect_to root_path
-          expect(gram.comments.length).to eq 1
-          expect(gram.comments.first.message).to eq "awesome gram"
-        end
-        
-        it "should require a user to be logged in to comment on a gram" do
-          gram = FactoryGirl.create(:gram)
-          post :create, params: { gram_id: gram.id, comment: { message: 'awesome gram' } }
-          expect(response).to redirect_to new_user_session_path
-        end
-        
-        it "should return http status 'not found' if gram is not found" do
-            user = FactoryGirl.create(:user)
-            sign_in user
-            post :create, params: { gram_id: 'YOLOSWAG', comment: { message: 'awesome message' } }
-            expect(response).to have_http_status :not_found
-        
-        end
-    end
+    
     
     describe "gram#destroy action" do
         it "shouln't let a user destroy a gram if the user didn't create the gram" do
